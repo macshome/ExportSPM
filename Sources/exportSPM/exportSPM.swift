d49a3f73ceeead6d9573d07ee9b1b684fdba3150
Josh Wisenbaker <macshome@mac.com>
2026-02-13 10:01:08 -0500

    Initial commit

--- Sources/exportSPM/exportSPM.swift
+++ Sources/exportSPM/exportSPM.swift
@@ -1,14 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
+import Foundation
 
 @main
 struct exportSPM: ParsableCommand {
+    @Argument(help: "Path to the .xcodeproj file.")
+    var xcodeprojPath = ""
+
     mutating func run() throws {
-        print("Hello, world!")
+            let projectURL = try getURL(xcodeprojPath)
+    }
+
+    func getURL(_ path: String) throws -> URL {
+        let projectURL = URL(fileURLWithPath: path + "/project.pbxproj")
+          guard try projectURL.checkResourceIsReachable() else {
+              throw ValidationError("Path to project file does not exist.")
+          }
+        return projectURL
     }
 }
---
+++ Tests/exportSPMTests/exportSPMTests.swift
@@ -0,0 +1,15 @@
+//
+//  exportSPMTests.swift
+//  exportSPM
+//
+//  Created by Josh Wisenbaker on 12/5/25.
+//
+
+import Foundation
+import Testing
+@testable import exportSPM
+
+
+@Test func testPathParsing() async throws {
+
+}
